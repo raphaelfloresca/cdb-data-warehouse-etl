8fb6f750a80414ec989255f4bfc1a6621eb94448
Raphael Angelo Floresca <rpngfl@gmail.com>
2023-03-20 15:06:57 +0800

    First commit

---
+++ helpers.py
@@ -0,0 +1,162 @@
+import requests
+import urllib.parse
+
+# Get data from an API
+def get_from_api(url, headers, endpoint=None):
+    request = requests.get(url, headers=headers)
+    json = request.json()
+    if not endpoint == None:
+        json = json[endpoint]
+        return json
+    return json
+
+
+# Filter out data without a particular key
+def filter_no_id(unfiltered_list, key_to_filter):
+    return [item for item in unfiltered_list if key_to_filter in item]
+
+
+# Take raw shares data and extract useful data points
+def transform_shares(row):
+    # Extract the first-level key value pairs
+    row_subset = {key: value for key, value in row.items()
+                  if key == "activity"
+                  or key == "created"
+                  or key == "text"
+                  or key == "lastModified"}
+
+    # Push required data into a dictionary
+    clean_dict = {}
+
+    # Schema: activity, created_actor, created_time, text, lastModified
+    clean_dict['activity'] = row_subset['activity']
+    clean_dict['created_actor'] = row_subset['created']['actor']
+    clean_dict['created_time'] = row_subset['created']['time']
+    clean_dict['text'] = row_subset['text']['text']
+    clean_dict['lastModified_time'] = row_subset['lastModified']['time']
+
+    return clean_dict
+
+
+# Get data from the next page
+def get_paged_data(start_data, headers):
+    next_data_list = []
+    next_page = next((item for item in start_data['paging']['links'] if item['rel'] == 'next'), None)
+
+    while not next_page is None:
+        # Next url
+        next_url = 'https://api.linkedin.com' + str(next_page['href'])
+
+        # Get next batch of data
+        next_data = get_from_api(next_url, headers)
+
+        # Filter out data without activity id
+        next_data_filtered = filter_no_id(next_data['elements'], 'activity')
+        next_flattened_data = list(map(transform_shares, next_data_filtered))
+
+        # Append flattened data to data list
+        next_data_list += next_flattened_data
+
+        # Find the next page url
+        next_page = next((item for item in next_data['paging']['links'] if item['rel'] == 'next'), None)
+
+    return next_data_list
+
+
+# Get a list of share IDs from a pandas Series of activity IDs
+def get_share_list(df, headers):
+    share_list = []
+
+    for activity in list(df['activity']):
+        get_share_urn_url = 'https://api.linkedin.com/v2/activities?ids=' + str(activity)
+        get_share_urn_request = requests.get(get_share_urn_url, headers=headers)
+        activity_to_share_urn = get_share_urn_request.json()['results'][str(activity)]['domainEntity']
+        share_list.append(activity_to_share_urn)
+
+    return share_list
+
+
+# Split a list into sub-lists of a given size: implemented as LinkedIn can only reliably pull in batches of 50
+def split(list_a, chunk_size):
+    for i in range(0, len(list_a), chunk_size):
+        yield list_a[i:i + chunk_size]
+
+
+# Encode a share list for batch GET request
+def encode_share_list(share_list):
+    encoded_list = [urllib.parse.quote(x) for x in share_list]
+    share_str = ','.join(encoded_list)
+    return share_str
+
+
+# Get social metadata for a split share list
+def get_chunked_social_metadata(split_share_list, headers):
+    # Get social metadata
+    all_social_metadata = []
+
+    for share_list in split_share_list:
+        share_str = encode_share_list(share_list)
+
+        get_social_metadata_url = 'https://api.linkedin.com/rest/socialMetadata?ids=List(' + str(share_str) + ')'
+
+        get_social_metadata_request = requests.get(get_social_metadata_url, headers=headers)
+        get_social_metadata_batch = get_social_metadata_request.json()
+
+        all_social_metadata.append(get_social_metadata_batch)
+
+    return all_social_metadata
+
+
+# Create a list of dictionaries containing social metadata for a given share ID
+def get_all_social_metadata(chunked_social_metadata):
+    social_metadata_list = []
+
+    # Iterate over chunks
+    for chunk in chunked_social_metadata:
+        # Iterate over each activity's social metadata
+        for share, metadata in chunk['results'].items():
+            social_metadata_row = {}
+
+            # Append activity to row
+            social_metadata_row['share'] = share
+            for key, value in metadata.items():
+                if key == 'reactionSummaries':
+                    if 'LIKE' in value:
+                        social_metadata_row.update({'like_count': value['LIKE']['count']})
+                    else:
+                        social_metadata_row.update({'like_count': 0})
+
+                    if 'PRAISE' in value:
+                        social_metadata_row.update({'praise_count': value['PRAISE']['count']})
+                    else:
+                        social_metadata_row.update({'praise_count': 0})
+
+                    if 'MAYBE' in value:
+                        social_metadata_row.update({'maybe_count': value['MAYBE']['count']})
+                    else:
+                        social_metadata_row.update({'maybe_count': 0})
+
+                    if 'EMPATHY' in value:
+                        social_metadata_row.update({'empathy_count': value['EMPATHY']['count']})
+                    else:
+                        social_metadata_row.update({'empathy_count': 0})
+
+                    if 'INTEREST' in value:
+                        social_metadata_row.update({'interest_count': value['INTEREST']['count']})
+                    else:
+                        social_metadata_row.update({'interest_count': 0})
+
+                    if 'APPRECIATION' in value:
+                        social_metadata_row.update({'appreciation_count': value['APPRECIATION']['count']})
+                    else:
+                        social_metadata_row.update({'appreciation_count': 0})
+
+                if key == 'commentSummary':
+                    social_metadata_row['comments_count'] = value['count']
+                    social_metadata_row['comments_top_level_count'] = value['topLevelCount']
+                if key == "commentsState":
+                    social_metadata_row['comments_state'] = value
+
+            social_metadata_list.append(social_metadata_row)
+
+    return social_metadata_list
---
+++ main.py
@@ -0,0 +1,66 @@
+import pandas as pd
+from datetime import date
+from helpers import *
+
+# Main entry point for the cloud function
+def pull_from_api(self):
+    # Start with first element
+    automations_api_url = 'https://connect.mailerlite.com/api/automations'
+
+    headers = {
+        "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI0IiwianRpIjoiOTMzMWMwZmMzZDRjMjQyOTU2NTU0NmE1M2IzNjQ3ZWI2NDgzOGUyNjcyZWU1MTRkMjFhZGQ2MjVlZTMwMjAyN2M5Nzg5NDU4YjZjYjdlMjgiLCJpYXQiOjE2NzkyODEwNjkuMzA5NjAxLCJuYmYiOjE2NzkyODEwNjkuMzA5NjAzLCJleHAiOjQ4MzQ5NTQ2NjkuMzAzODkyLCJzdWIiOiIzMTA3MDIiLCJzY29wZXMiOltdfQ.eV66qRiAJ7Xk0-pEvcNL9DuLF9EkV7jonvmdGhZ_53o8_ErxHIN4woXqW5xSj784kPS5g7gJzfSUfpP-7pwkp1KUDEfxHpwROQf2tKjQA3SxOUSz64qC7X7jkfe490RL9WJiLUE3NaIfqoLBGOrdI4KH-k6onkAxVzS-DdFXYkJMCner30YjStD7mt8UyqQQqXeuvOrtRkonIGzvj_-faQVG-DtNZrfA4-EilRssvKuvBisxwQWwKn9PWds1Jqyetn7vnb9Fgll7gZomGO0sYMcZmfFD7lD6eGnXAPKA6AJ1IGS5o-OI0UcvsdQmREpbstO9FfeC2qxfK_8GJlaOjE2XEagnGexC-p_yz7jZNtT2ki-ueFaVeglhzLPfgzYCubc8fyrrP9SeCmV99FilarLpmHjOW7mWmihhLqI4y4-L1evVpJP8_gkOZhe6earyH0UXuu0ZiGczvtTQYnyEbKLNd80qA5bVBNY1LAMEc8qw67Dmm6AhphA96VFInzIlgnIvOvfCd6Rd88Gl04llkg_7hYH3hfXNqeYxWkU-mYhcWIDh1LRqpHue6L8CnWQMIz9Tiz46QSVVOrCPFUQvcSUAYwfLQmuW_uG32v6D5FqOz5K2StCWTrlxb5w55iNF6ioltkjHVtjOKyP2PdlCnCyj_6CPBgJSZHQYa-sv1rM",
+    }
+
+    # Get all data from API
+    automations_data = get_from_api(automations_api_url, headers, 'data')
+
+    # Get data from all automations
+    automations_names = []
+    automations_stats = []
+
+    for data in automations_data:
+        automations_names.append(data['name'])
+        automations_stats.append(data['stats'])
+
+    automations_names_series = pd.Series(automations_names)
+    df = pd.json_normalize(automations_stats)
+    df['name'] = automations_names_series
+    # All data pulled associated with the date of the API request - allows for partitioning later
+    df["pull_date"] = pd.to_datetime(date.today())
+
+    cols = ['name',
+            'pull_date',
+            'completed_subscribers_count',
+            'subscribers_in_queue_count',
+            'sent',
+            'opens_count',
+            'unique_opens_count',
+            'clicks_count',
+            'unique_clicks_count',
+            'unsubscribes_count',
+            'spam_count',
+            'hard_bounces_count',
+            'soft_bounces_count',
+            'social_interactions_count',
+            'bounce_rate.float',
+            'click_to_open_rate.float',
+            'open_rate.float',
+            'open_rate.string',
+            'click_rate.float',
+            'click_rate.string',
+            'unsubscribe_rate.float',
+            'spam_rate.float',
+            'spam_rate.string',
+            'hard_bounce_rate.float',
+            'soft_bounce_rate.float',
+            'forward_rate.float',
+            'forward_rate.string',
+            'social_interaction_rate.float']
+
+    # Reordered columns
+    df = df[cols]
+
+    # Write dataframe to csv
+    df.to_csv('test.csv', encoding='utf-8')
+
+    return "Data has been saved"
