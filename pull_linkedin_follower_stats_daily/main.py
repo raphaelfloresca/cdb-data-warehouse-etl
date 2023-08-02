bdeaa3ee16872d132b345c657bd89ab7e67e8824
Raphael <rpngfl@gmail.com>
2023-08-02 15:55:08 +0800

    follower_stats_daily: first commit

---
+++ pull_linkedin_follower_stats_daily/main.py
@@ -0,0 +1,159 @@
+import pandas as pd
+import sys
+import json
+import requests
+import pandas_gbq
+import gcsfs
+import os
+from datetime import date
+from google.cloud import bigquery
+from helpers import return_active_token, get_from_api, create_bq_table
+from flatten_json import flatten
+from datetime import date, datetime, timedelta
+import calendar
+
+
+'''
+function 1: All this function is doing is responding and validating any HTTP
+request, this is important if you want to schedule an automatic refresh or test
+the function locally.
+'''
+
+
+def validate_http(request):
+
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+
+    if request_json and request_args:
+        pull_to_prod()
+        return f'Data pull complete'
+    elif request_json and request_args:
+        pull_to_prod()
+        return f'Data pull complete'
+    else:
+        pull_to_prod()
+        return f'Data pull complete'
+
+
+'''
+function 2: This is the code to pull data from the API and store it in a DataFrame
+'''
+
+def get_api_data():
+
+    # Calculate time
+    end_date = date.today() - timedelta(3)
+    start_date = date.today() - timedelta(4)
+    midnight_time = datetime.min.time()
+
+    end_datetime = datetime.combine(end_date, midnight_time)
+    start_datetime = datetime.combine(start_date, midnight_time)
+
+    time_range_start = calendar.timegm(start_datetime.utctimetuple()) * 1000
+    time_range_end = calendar.timegm(end_datetime.utctimetuple()) * 1000
+
+    # Fetch daily data from follower statistics API
+    url = "https://api.linkedin.com/rest/organizationalEntityFollowerStatistics?q=organizationalEntity&organizationalEntity=urn:li:organization:30216658&timeIntervals.timeGranularityType=DAY&timeIntervals.timeRange.start={}&timeIntervals.timeRange.end={}".format(time_range_start, time_range_end)
+
+    # Headers
+    headers = {
+        "Authorization": "Bearer {}".format(os.environ.get("TOKEN")),
+        'Linkedin-Version': '202302'
+    }
+
+    # Get all data from API
+    data = get_from_api(url, headers)
+    endpoint = data['elements']
+
+    # Flatten json
+    daily_data_flatten = map(flatten, endpoint)
+
+    # Create dataframe
+    df = pd.DataFrame(list(daily_data_flatten))
+
+    # Assign new column names
+    old_col_names = ['followerGains_organicFollowerGain',
+                     'followerGains_paidFollowerGain',
+                     'organizationalEntity',
+                     'timeRange_start',
+                     'timeRange_end']
+
+    new_col_names = ['organic_follower_gain',
+                     'paid_follower_gain',
+                     'organization',
+                     'time_range_start',
+                     'time_range_end']
+
+    new_cols = dict(zip(old_col_names, new_col_names))
+    df = df.rename(columns=new_cols)
+
+    # Convert last modified time to datetime format
+    df['time_range_start'] = pd.to_datetime(df['time_range_start'], unit='ms')
+    df['time_range_end'] = pd.to_datetime(df['time_range_end'], unit='ms')
+
+    return df
+
+
+'''
+function 3: This pulls the data to the production database
+'''
+
+
+def pull_to_prod():
+
+    df = get_api_data()
+    bq_load('linkedin_follower_stats_daily', df, 'cdb_marketing_data')
+
+    return "Data has been loaded to BigQuery"
+
+
+'''
+function 4: This pulls the data to the staging database - used for testing
+'''
+
+
+def pull_to_staging():
+
+    schema = [
+        bigquery.SchemaField("organic_follower_gain", "INTEGER", mode="REQUIRED"),
+        bigquery.SchemaField("paid_follower_gain", "INTEGER", mode="REQUIRED"),
+        bigquery.SchemaField("organization", "STRING", mode="REQUIRED"),
+        bigquery.SchemaField("time_range_start", "DATE", mode="REQUIRED"),
+        bigquery.SchemaField("time_range_end", "DATE", mode="REQUIRED"),
+    ]
+
+    table = create_bq_table(schema)
+    df = get_api_data()
+    bq_load(table, df, 'marketing_staging')
+
+    return "Data has been loaded to BigQuery"
+
+
+'''
+function 5: This pulls the data to a csv - used for testing
+'''
+
+
+def pull_to_csv():
+
+    df = get_api_data()
+    df.to_csv("test.csv")
+
+    return "Data has been saved"
+
+
+'''
+function 6: This function just converts your pandas dataframe into a bigquery
+table, you'll also need to designate the name and location of the table in the
+variable names below.
+'''
+
+
+def bq_load(key, value, dataset_name):
+
+    project_name = 'marketing-bd-379302'
+    table_name = key
+
+    value.to_gbq(destination_table='{}.{}'.format(dataset_name, table_name),
+                 project_id=project_name, if_exists='append')
